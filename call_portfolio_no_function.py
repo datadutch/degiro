2cc5104987011c9db3ee6ce307f1bbb1ef559278
unknown <john.van.der.kooij@enexis.nl>
2022-02-05 19:22:48 +0100

    fixing

--- call_portfolio_no_function.py
+++ call_portfolio_no_function.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import sqlite3
 con = sqlite3.connect('degiro.db')
 
@@ -23,3 +24,7 @@ portfolio[tickerinfo.columns] = tickerinfo
 
 del(tickerinfo)
 degiro.logout()
+=======
+mport sqlite3#(lf)con = sqlite3.connect('degiro.db')#(lf)#(lf)import json#(lf)import pandas as pd#(lf)import degiroapi#(lf)from degiroapi.product import Product#(lf)from degiroapi.order import Order#(lf)from degiroapi.utils import pretty_json#(lf)#(lf)with open('c:\git\degiro\config.json','r') as file:#(lf)    data = json.load(file)#(lf)    user = data['degiro_user']#(lf)    password  = data['degiro_password']#(lf)#(lf)degiro = degiroapi.DeGiro()#(lf)degiro.login(user, password)#(lf)portfolio = pd.DataFrame(degiro.getdata(degiroapi.Data.Type.PORTFOLIO, False))#(lf)#(lf)tickerinfo = portfolio.apply(lambda row : degiro.product_info(row['id']), axis = 1, result_type = 'expand')#(lf)#(lf)portfolio[tickerinfo.columns] = tickerinfo#(lf)#(lf)del(tickerinfo)#(lf)degiro.logout()"),
+    portfolio1 = Source{[Name="portfolio"]}[Value],
+>>>>>>> cec6454 (updates)
